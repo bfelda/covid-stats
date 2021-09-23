191dda37a8064a2ad61178bbe1d0e6ddd76a9d8e
bfelda <bfelda@gmail.com>
2021-09-23 11:29:28 -0500

    added analytics

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import ReactGA from "react-ga";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
@@ -10,8 +11,14 @@ ReactDOM.render(
 	</React.StrictMode>,
 	document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log);
+ReactGA.initialize("G-XXDP3NJT3K");
+reportWebVitals(({ id, name, value }) =>
+	ReactGA.event({
+		action: name,
+		category: "Web Vitals",
+		label: id,
+		nonInteraction: true,
+		value: Math.round(name === "CLS" ? value * 1000 : value),
+	})
+);
+// reportWebVitals(console.log);
