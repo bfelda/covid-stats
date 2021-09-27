e27ff35e133d8ec8f14a987596189dedc91ccd3e
bfelda <bfelda@gmail.com>
2021-09-26 19:06:35 -0500

    updated function call

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,3 +1,27 @@
-exports.handler = (event, context, callback) => {
-	console.log("sms requested");
+// submission-created.js
+
+// initialize the helper library client
+const client = require("twilio")(
+	process.env.TWILIO_ACCOUNT_SID,
+	process.env.TWILIO_AUTH_TOKEN
+);
+
+exports.handler = function (event, context, callback) {
+	console.log(JSON.parse(event.body));
+	// Promise.all(
+	// 	// split the string of several messages into single numbers
+	// 	// send message to each of them
+	// 	event..split(";").map((num) => {
+	// 		return client.messages.create({
+	// 			from: process.env.BOT_NUMBER,
+	// 			to: num,
+	// 			body: process.env.BOT_MESSAGE,
+	// 		});
+	// 	})
+	// )
+	// 	.then(() => callback(null, { statusCode: 200, body: "Created" }))
+	// 	.catch((e) => {
+	// 		console.log(e);
+	// 		callback(e);
+	// 	});
 };
